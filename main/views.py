d9ee8b14000f72c393448b41ef6206674be94d11
webdevmrpl <litvinenko.webdev@gmail.com>
2020-08-04 18:35:08 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -24,12 +24,12 @@ def index(request):
     return render(request, "main/index.html", context)
 
 
-# def by_rubric(request, rubric_id):
-#     bbs = Bb.objects.filter(rubric=rubric_id)
-#     rubrics = Rubric.objects.all()
-#     current_rubric = Rubric.objects.get(pk=rubric_id)
-#     context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
-#     return StreamingHttpResponse(render_to_string("main/by_rubric.html", context, request))
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return StreamingHttpResponse(render_to_string("main/by_rubric.html", context, request))
 
 
 def detail_view(request, pk):
