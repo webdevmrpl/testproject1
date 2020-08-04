189e3893e4b88c04d1030e8a407c8ad8bf6d8272
webdevmrpl <litvinenko.webdev@gmail.com>
2020-08-04 19:03:56 +0300

    Update urls.py

--- training/urls.py
+++ training/urls.py
@@ -11,5 +11,3 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.THUMBNAIL_MEDIA_URL, document_root=settings.THUMBNAIL_MEDIA_ROOT)
-
-#Another big cock
